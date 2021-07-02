800668f6297eb1a38186b178152d1d6efe782fde
tronixtek <francisvictor.st@gmail.com>
2021-07-02 16:33:08 +0100

    new comit

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
+const {api} = require("./helpers/keys");
 const express = require("express");
 const app = express();
-const cors = require("cors");
+const cors = require("cors")
+const morgan = require("morgan");
+const {connection} = require("./db")
+
+
+//Routers
+const home = require("./Route/home");
+const users = require("./Route/user");
+const admin = require("./Route/admin");
+
+
+
+//connecting to database
+connection();
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors())
+app.use(`${api}/`,home);
+app.use(`${api}/`,users);
+app.use(`${api}/`,admin);
 
+//error handler
+app.use((error, req, res, next) => {
+    res.status(500).json({ error: error.message });
+  });
+  
 
-app.get("/home",(req,res)=>{
-    res.json("welcome home")
-})
+PORT= 5000 || process.env.PORT;
+app.listen(PORT,console.log(`App Running on ${PORT}`));
 
-PORT = process.env.PORT || 4000;
-app.listen(PORT,console.log("APP RUNNING"))
+module.exports = app;
+
